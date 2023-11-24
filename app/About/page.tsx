3781d576fa65dad8dc53badfd60a9bb51fd8ec4a
isiagi <isiagigeofrey0@gmail.com>
2023-11-24 15:15:31 +0300

    chore: Add about us page

---
+++ app/About/page.tsx
@@ -0,0 +1,12 @@
+import HeaderBanner from "@/components/headerBanner/HeaderBanner";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <HeaderBanner name="About Us" />
+    </div>
+  );
+}
+
+export default page;
