7086a6cec9dfacb33cd9358987c13212837d9eed
isiagi <isiagigeofrey0@gmail.com>
2023-11-24 15:30:59 +0300

    chore: Adds why component.

--- app/home/Page.tsx
+++ app/home/Page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/hero/Hero";
 import Team from "@/components/team/Team";
 import Testimonial from "@/components/testmon/Testimonial";
+import Why from "@/components/why/Why";
 import React from "react";
 
 function Page() {
   return (
     <>
       <Hero />
+      <Why />
       <Testimonial />
       <Team />
     </>
---
+++ components/why/Why.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Why() {
+  return (
+    <div className="py-10">
+      <div className="grid grid-cols-fluid px-5">
+        <div>
+          <p>Why choose us</p>
+          <h3>It takes two flints to make a fire.</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+
+          <button type="button">Book Session</button>
+        </div>
+        <div className="grid grid-cols-fluid gap-2">
+          <div className="border-[1px] p-4">
+            <h3>It takes two</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus,
+            </p>
+          </div>
+          <div className="border-[1px] p-4">
+            <h3>It takes two</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus,
+            </p>
+          </div>
+          <div className="border-[1px] p-4">
+            <h3>It takes two</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus,
+            </p>
+          </div>
+          <div className="border-[1px] p-4">
+            <h3>It takes two</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus,
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Why;
