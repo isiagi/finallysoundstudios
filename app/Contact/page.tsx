7e5ec875b85e62c1502a3bcd9575338b8b5fb3d1
isiagi <isiagigeofrey0@gmail.com>
2023-12-18 13:58:19 +0300

    styling

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -18,7 +18,7 @@ function page() {
         </div>
         <div className="container px-5 py-24 mx-auto flex">
           <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+            <h2 className="text-[#AD8987] text-lg mb-1 font-medium title-font">
               Talk To Us!
             </h2>
             <p className="leading-relaxed mb-5 text-gray-600">
